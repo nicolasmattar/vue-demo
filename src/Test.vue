464f5cf8003dcc8e14b82651ee99fcbbc6eb4b93
nicolasmattar <nicolasmattar@gmail.com>
2017-01-20 00:14:36 -0300

    First Commit

---
+++ src/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <hr/>
+        <h1>{{ msg }}</h1>
+        Este es otro componente!!!!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data () {
+            return {
+                msg: 'Testing property like Hello World but *special*!!'
+            }
+        }
+    }
+</script>
